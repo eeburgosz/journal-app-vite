0a2efdee0c1b39b22f3927817a7066af68518c23
eeburgosz <hdst.bur@gmail.com>
2023-01-24 16:39:13 -0300

    Activando nota creada previamente

--- src/journal/components/SidebarItem.jsx
+++ src/journal/components/SidebarItem.jsx
@@ -7,8 +7,16 @@ import {
   ListItemText,
 } from "@mui/material";
 import { useMemo } from "react";
+import { useDispatch } from "react-redux";
+import { setActiveNote } from "../../store/journal/journalSlice";
 
-export const SidebarItem = ({ title, body, id }) => {
+export const SidebarItem = ({ title, body, id, date, imageUrls = [] }) => {
+  const dispatch = useDispatch();
+
+  const onClickNote = () => {
+    dispatch(setActiveNote({ title, body, id, date, imageUrls }));
+  };
+
   //! Hook para que el title se vea mejor si es muy largo--------------------------
   const newTitle = useMemo(() => {
     return title.length > 17 ? title.substring(0, 17) + "..." : title;
@@ -17,7 +25,7 @@ export const SidebarItem = ({ title, body, id }) => {
 
   return (
     <ListItem disablePadding>
-      <ListItemButton>
+      <ListItemButton onClick={onClickNote}>
         <ListItemIcon>
           <TurnedInNot />
         </ListItemIcon>
